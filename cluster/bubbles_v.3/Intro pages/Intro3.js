cf3122f474539574b2bfade760b31ae575038d8e
squeakyheatr <macuser@Roberts-Macbook-Air-2.local>
2015-12-03 17:34:46 -0800

    Made 2 seperate pages for the into, Introduction2.html needs a link to the next page where I wrote filenamehere

---
+++ cluster/bubbles_v.3/Intro pages/Intro3.js
@@ -0,0 +1,25 @@
+
+// Simple bubble program.
+var intro2 = "Count the yellow balls";
+
+// Our colors, to be stored in an array. 
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+// Array of Colors
+//var letterColors = [red,orange,green,blue,purple];
+// circle and sqaure are our only options. 
+var bubbleShape = "circle";
+
+
+// var myname="Chris!";
+drawName(intro2, letterColors);
+//drawName(myName, letterColors);
+
+
+
+bounceBubbles();
---
+++ cluster/bubbles_v.3/Intro pages/Introduction.js
@@ -0,0 +1,21 @@
+// Simple bubble program.
+var intro = "IRO COLOR GAME";
+
+// Our colors, to be stored in an array. 
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+// Array of Colors
+//var letterColors = [red,orange,green,blue,purple];
+// circle and sqaure are our only options. 
+var bubbleShape = "circle";
+
+
+// var myname="Chris!";
+drawName(intro, letterColors);
+
+bounceBubbles();
